794783f6e2d9ea890d678f6fd5c62223cadea7b6
Cleophas <kalekemcleophas01@gmail.com>
2021-05-12 18:01:54 -0400

    first commit

---
+++ helloWorld.java
@@ -0,0 +1,13 @@
+public class helloWorld{
+
+	public static void main (String[] args){
+		System.out.println("Hello Jenkins");
+
+		int a = 10;
+		int b = 20;
+
+		int total = a + b;
+
+		System.out.println("The total is " + total);
+	}
+}
